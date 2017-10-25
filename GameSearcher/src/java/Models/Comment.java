f83f5032ce7e6ab5ed236e98075532bee90239cc
Christopher McArthur <prince.chrismc@gmail.com>
2017-10-25 15:14:22 -0400

    adding base Comment model

---
+++ GameSearcher/src/java/Models/Comment.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author cmcarthur
+ */
+public class Comment {
+    private String comment_id;
+    private String user_id;
+    private String game_id;
+    
+    private String data;
+    private String details;
+    private String rating;
+
+    /**
+     * @return the comment_id
+     */
+    public String getComment_id() {
+        return comment_id;
+    }
+
+    /**
+     * @param comment_id the comment_id to set
+     */
+    public void setComment_id(String comment_id) {
+        this.comment_id = comment_id;
+    }
+
+    /**
+     * @return the user_id
+     */
+    public String getUser_id() {
+        return user_id;
+    }
+
+    /**
+     * @param user_id the user_id to set
+     */
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    /**
+     * @return the game_id
+     */
+    public String getGame_id() {
+        return game_id;
+    }
+
+    /**
+     * @param game_id the game_id to set
+     */
+    public void setGame_id(String game_id) {
+        this.game_id = game_id;
+    }
+
+    /**
+     * @return the data
+     */
+    public String getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the details
+     */
+    public String getDetails() {
+        return details;
+    }
+
+    /**
+     * @param details the details to set
+     */
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    /**
+     * @return the rating
+     */
+    public String getRating() {
+        return rating;
+    }
+
+    /**
+     * @param rating the rating to set
+     */
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
