12c06d7ff13d71d96b1df5d297dade12bef9a27c
Christopher McArthur <prince.chrismc@gmail.com>
2017-10-25 12:25:20 -0400

    unable to get dispatcher to work

---
+++ GameSearcher/test/Controllers/GrettingController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author cmcarthur
+ */
+@Controller
+public class GrettingController {
+    @RequestMapping("/greeting.html")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
