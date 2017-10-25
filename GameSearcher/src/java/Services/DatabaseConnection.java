d807e9b2c331faf1b41ba6b6703be15acc8fd9b8
Christopher McArthur <prince.chrismc@gmail.com>
2017-10-25 13:43:54 -0400

    mini sql connection wrapper

---
+++ GameSearcher/src/java/Services/DatabaseConnection.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author cmcarthur
+ * @inspiration https://stackoverflow.com/a/10916633/8480874
+ */
+public class DatabaseConnection {
+    private static final String urlstring = "jdbc:mysql://localhost:3306/prototypeeop";    
+    private static final String driverName = "com.mysql.jdbc.Driver";   
+    private static final String username = "root";   
+    private static final String password = "triala";
+    
+    private static Connection con;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(driverName);
+            try {
+                con = DriverManager.getConnection(urlstring, username, password);
+            } catch (SQLException ex) {
+                // log an exception. fro example:
+                System.out.println("Failed to create the database connection."); 
+            }
+        } catch (ClassNotFoundException ex) {
+            // log an exception. for example:
+            System.out.println("Driver not found."); 
+        }
+        return con;
+    }
+}
