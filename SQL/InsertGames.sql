868a2eccb4a210f79a45595d9ac9be19f1dc3c36
Chris McArthur <prince.chrismc@gmail.com>
2017-11-01 21:38:32 -0400

    corrected date parse/converter

--- SQL/InsertGames.sql
+++ SQL/InsertGames.sql
@@ -1,7 +1,7 @@
 INSERT INTO gamesearcher.games (gameName, gameDesc, gameConsoles, gamePlayers, gameCoop, gameGenres, gameReleaseDate, gameDeveloper, gamePublisher,
 gameFrontArtUrl, gameBackArtUrl, gameLogoArtUrl, gameDevLogoArtUrl, gamePrice, gameDiscount) VALUES
 (
-'007: The World is not Enough', 'overview to come', 'PS;NS64;GBColor;', 1, false, 'Shooter;', STR_TO_DATE('11-08-2000', '%d-%m-%Y'), 'Black Ops Entertainment', /*https://stackoverflow.com/a/9907257/8480874*/
+'007: The World is not Enough', '', 'PS;', 1, false, 'Shooter;', STR_TO_DATE('11/08/2000', '%m/%d/%Y'), 'Black Ops Entertainment', /*https://stackoverflow.com/a/9907257/8480874*/
 'Electronic Arts', 'http://thegamesdb.net/banners/boxart/original/front/11554-1.jpg', 'http://thegamesdb.net/banners/boxart/original/back/11554-1.jpg', 
 'http://thegamesdb.net/banners/clearlogo/11554.png', '', 5.99, 0.30
 );
