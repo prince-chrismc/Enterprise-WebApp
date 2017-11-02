deec03432bf57ca0c6a0b230bc2f37a021e34a37
Chris McArthur <prince.chrismc@gmail.com>
2017-11-01 22:45:23 -0400

    insert script for the first 14 games

--- SQL/InsertGames.sql
+++ SQL/InsertGames.sql
@@ -3,5 +3,62 @@ gameFrontArtUrl, gameBackArtUrl, gameLogoArtUrl, gameDevLogoArtUrl, gamePrice, g
 (
 '007: The World is not Enough', '', 'PS;', 1, false, 'Shooter;', STR_TO_DATE('11/08/2000', '%m/%d/%Y'), 'Black Ops Entertainment', /*https://stackoverflow.com/a/9907257/8480874*/
 'Electronic Arts', 'http://thegamesdb.net/banners/boxart/original/front/11554-1.jpg', 'http://thegamesdb.net/banners/boxart/original/back/11554-1.jpg', 
-'http://thegamesdb.net/banners/clearlogo/11554.png', '', 5.99, 0.30
+'http://thegamesdb.net/banners/clearlogo/11554.png', NULL, 5.99, 0.30
+),
+(
+'2Xtreme', '' , 'PS;' , 2 , false , 'Racing;Sports;' , STR_TO_DATE('10/31/1996', '%m/%d/%Y') , 'Sony' , 'SCEA' , 
+'http://thegamesdb.net/banners/boxart/original/front/8537-1.png' , NULL , 'http://thegamesdb.net/banners/clearlogo/8537.png' , 
+'http://thegamesdb.net/banners/_gameviewcache/publishers/0-sony.png' , 4.49 , 0.00
+),
+(
+'Driver', '' , 'PS;PC;' , 1 , false , 'Action;Racing;' , STR_TO_DATE('07/08/1999', '%m/%d/%Y') , 'Reflections Interactive Limited' , 'GT Interactive' , 
+'http://thegamesdb.net/banners/boxart/original/front/1546-1.jpg' , 'http://thegamesdb.net/banners/boxart/original/back/1546-1.jpg' , 
+'http://thegamesdb.net/banners/clearlogo/1546.png' , NULL , 4.99 , 0.25
+),
+(
+'Drive 2', '' , 'PS;' , 2 , false , 'Action;Racing;' , STR_TO_DATE('11/13/2000', '%m/%d/%Y') , 'Reflections Interactive' , 'Infogrames' , 
+'http://thegamesdb.net/banners/boxart/original/front/521-2.jpg' , 'http://thegamesdb.net/banners/boxart/original/back/521-1.jpg' , 
+'http://thegamesdb.net/banners/clearlogo/521.png' , NULL , 4.99 , 1.00
+),
+(
+'Spyro: Year of the Dragon', 'Spyro: Year of the Dragon is the third installment in the Spyro series and the last Spyro game to be released for the PlayStation. The game was the last Spyro game Insomniac developed; their next title would be Ratchet & Clank for the PlayStation 2. This is the only Spyro game in the PlayStation library to not get released in Japan. Named after the animal of the Chinese zodiac, which was the symbol at the time of the game\'s release, Year of the Dragon follows the titular purple character Spyro as he travels to the "Forgotten Worlds" after 150 magical dragon eggs are stolen from the land of the dragons by an evil sorceress. Players travel across thirty different worlds gathering gems and eggs. Year of the Dragon introduced new characters and minigames to the series, as well as offering improved graphics and music. Upon release, the game sold more than two million units in the United States,and received positive critical response. Reviewers noted the game built on the successful formula of its predecessors.',
+ 'PS;' , 1 , false , 'Platform;' , STR_TO_DATE('10/23/2000', '%m/%d/%Y') , 'Insomniac Games' , 'Sony' , 'http://thegamesdb.net/banners/boxart/original/front/4986-1.jpg' , 
+ 'http://thegamesdb.net/banners/boxart/original/back/4986-1.jpg' , NULL , 'http://thegamesdb.net/banners/_gameviewcache/publishers/0-insomniacgames.jpeg' , 5.49 , 0.00
+ ),
+(
+'Jak 3', 'Jak 3 is set in the New World, its time frame placing it around 300 years after the events of the first game. Jak 3 largely focuses on the Wasteland, a large desert only briefly referred to in the previous entry in the series as being completely uninhabited and inhospitable. Spargus City, a large settlement within the Wasteland bordering the ocean, is where the game officially begins, and serves as a hub for the player, where new weapons and upgrades can be earned, and most missions are given out.',
+ 'PS2;' , 1 , false , 'Action;Platform;Shooter;' , STR_TO_DATE('11/09/2004', '%m/%d/%Y') , 'Naughty Dog' , 'Sony Computer Entertainment' , 
+ 'http://thegamesdb.net/banners/boxart/original/front/4872-1.jpg' , 'http://thegamesdb.net/banners/boxart/original/back/4872-1.jpg' , NULL , 
+ 'http://thegamesdb.net/banners/_gameviewcache/publishers/0-Naughty_Dog_Logo.png' , 8.49 , 0.00
+ ),
+(
+'Jak and Dexter:The Precursor Legacy', 'Jak and Daxter: The Precursor Legacy offers several collectibles for the player to gather. The main objects are "Power Cells", used to energize machinery for use in reaching new levels. Egg-shaped "Precursor Orbs" function as currency, and can be traded in for Power Cells at several locations in the game, while collecting all seven "Scout Flies" in any level rewards the player with a Power Cell. While Precursor Orbs are present in all Jak and Daxter games, Power Cells would not make a reappearance until Jak 3, where they appear in artifact collecting missions.',
+'PS2;PS4;' , 1 , false , 'Action;Platform;' , STR_TO_DATE('12/03/2001', '%m/%d/%Y') , 'Naughty Dog' , 'Sony Computer Entertainment' , 
+'http://thegamesdb.net/banners/boxart/original/front/4865-1.jpg' , 'http://thegamesdb.net/banners/boxart/original/back/4865-1.jpg' , 
+'http://thegamesdb.net/banners/clearlogo/4865.png' , 'http://thegamesdb.net/banners/_gameviewcache/publishers/0-Naughty_Dog_Logo.png' , 7.99 , 0.00
+),
+(
+'Jak II', 'The game begins with Jak and Daxter, the protagonists, and Samos, their guardian, watching as Keira, Samos’ daughter and Jak’s love interest, demonstrate the Rift Rider and Precursor Rift Gate that they found in the previous game. When activated, the Rift opens a portal, and a large ominous atmosphere and a monster appear. Jak launches the Rider through the gate, past the monster. Jak and Daxter, separated from Keira and Samos, arrive in the segregated dystopia known as Haven City. They are separated on arrival, and Jak is captured by the city\'s Krimzon Guard. While Jak is imprisoned, he undergoes experiments with Dark Eco under the command of the city\'s leader: Baron Praxis. Jak, along with previous prisoners in the city jail, were experimented on for Operation: Dark Warrior, a project that consisted of injecting prisoners with dark eco in order to give them super-human abilities to be used in Haven City\'s war against animalistic creatures called Metal Heads.' , 'PS2;' , 1 , false , 'Action;Adventure;Platform;Shooter;' , STR_TO_DATE('10/14/2003', '%m/%d/%Y') , 'Naughty Dog' , 'Sony Computer Entertainment' , 'http://thegamesdb.net/banners/boxart/original/front/4867-1.jpg' , 'http://thegamesdb.net/banners/boxart/original/back/4867-1.jpg' , 'http://thegamesdb.net/banners/clearlogo/4867.png' , 'http://thegamesdb.net/banners/_gameviewcache/publishers/0-Naughty_Dog_Logo.png' , 7.49 , 0.00
+),
+(
+'Jak X : Combat Racing', 'After saving Haven City once again, Jak and Daxter find themselves poisoned forced into competition on the international car-racing circuit by the execution of Krew\'s will. Featuring over a dozen customizable vehicles designed with unique handling characteristics, players will race their whip through 20 tracks in eight different terrain arenas. With an assortment of power-ups available, including projectile weapons, mines, ranged weapons, turbo boosts, shields, and smart bombs, players should have the means necessary to ensure they\'re the first to cross the finish line. The game supports both offline and online multiplayer. Offline multiplayer is the typical two-player split-screen multiplayer. Online multiplayer is playable with up to five other players. Both modes feature the same game types, which are the same as the single player except that human players are replacing the AI.' , 'PS2;' , 1 , false , 'Action;Racing; Shooter;' , STR_TO_DATE('10/18/2005', '%m/%d/%Y') , 'Naughty Dog' , 'Sony Computer Entertainment' , 'http://thegamesdb.net/banners/boxart/original/front/9720-1.jpg' , 'http://thegamesdb.net/banners/boxart/original/back/9720-1.jpg' , NULL , 'http://thegamesdb.net/banners/_gameviewcache/publishers/0-Naughty_Dog_Logo.png' , 5.49 , 0.00
+),
+(
+'James Bond 007: Nightfire', 'The game\'s prologue mission starts in Paris, France, with James Bond (voiced by Maxwell Caulfield with the likeness of Pierce Brosnan) helping French Intelligence Operative Dominique Paradis evade a gang chase while chasing a truck with a stolen nuclear weapon, before continuing in his car. After stopping the truck from blowing up the Eiffel Tower, Dominique and James celebrate the new year.' , 'PS2;XBOX;PC;' , 4 , false , 'Shooter;' , STR_TO_DATE('11/18/2002', '%m/%d/%Y') , 'Eurocom' , 'Electronic Arts' , 'http://thegamesdb.net/banners/boxart/original/front/4757-1.jpg' , 'http://thegamesdb.net/banners/boxart/original/back/4757-1.jpg' , 'http://thegamesdb.net/banners/clearlogo/4757.png' , 'http://thegamesdb.net/banners/_gameviewcache/publishers/0-Eurocom.png' , 7.49 , 0.00
+),
+(
+'Juiced', 'The player can choose a wide variety of car modifications, such as visual modifications, performance modifications and more. All of these customization options provide a personalized and unique car to suit the player\’s needs. The gameplay offered to the player includes a career mode, sprint, race, solo, cruise, and a new show-off mode where the player does tricks with his or her car to keep the crowd entertained.' , 'PS2;XBOX;PC;' , 1 , false , 'Racing;' , STR_TO_DATE('06/15/2005', '%m/%d/%Y') , 'Juice Games' , 'THQ' , 'http://thegamesdb.net/banners/boxart/original/front/4860-1.jpg' , 'http://thegamesdb.net/banners/boxart/original/back/4860-1.jpg' , NULL , 'http://thegamesdb.net/banners/_gameviewcache/publishers/0-logo_USGames.jpg' , 5.99 , 0.00
+),
+(
+'Killzone', 'The game is set in an era of space colonization where the Helghast Empire has recovered from its defeat in the First Helghan War and launched a blitzkrieg against the outer Interplanetary Strategic Alliance (I.S.A.) colony planet Vekta. Vekta\'s orbital Strategic Defense (S.D.) platforms failed during the initial assault, allowing the Helghast to land swarms of soldiers onto the surface and making it all the more difficult for the outnumbered I.S.A. forces. In the game, the Helghast are a faction of human descendants who colonized the planet Helghan many generations ago. The planet\'s harsh environment forced the Helghast to adapt and mutate so much that they can no longer be considered human.' , 'PS2;' , 1 , false , 'Shooter;' , STR_TO_DATE('11/02/2004', '%m/%d/%Y') , 'Guerrilla Games' , 'Sony Computer Entertainment Europe' , 'http://thegamesdb.net/banners/boxart/original/front/35-2.jpg' , 'http://thegamesdb.net/banners/boxart/original/back/35-1.jpg' , NULL , NULL , 7.99 , 0.00
+),
+(
+'LEGO Indiana Jones: The Original Adventures', 'In LEGO Indiana Jones: The Original Adventures the players get to explore the Indiana Jones universe with "LEGO-fied" characters. The game focuses on the three original films: Raiders of the Lost Ark, Indiana Jones and the Temple of Doom, and Indiana Jones and the Last Crusade, each film consisting of six separate chapters retelling the stories in a humorous, light-hearted way that tones down the violence while removing some of the more overt religious and political references found in the film.' ,
+'PS2;PS3;XBOX360;PC;' , 2 , true , 'Action;Platform;' , STR_TO_DATE('06/03/2008', '%m/%d/%Y') , 'Traveller\'s Tales' , 'LucasArts' , 'http://thegamesdb.net/banners/boxart/original/front/10831-1.jpg' , 'http://thegamesdb.net/banners/boxart/original/back/10831-1.jpg' , 'http://thegamesdb.net/banners/clearlogo/10831.png' , NULL , 5.99 , 0.50
+),
+(
+'Lego Star Wars: The Video Game', 'Gameplay in Lego Star Wars is geared towards family play, and as such does not feature a game over scenario. Given a specific set of characters in each scenario, based on a scene from each of the movies, up to two players can control them, using their different abilities. By walking up to another friendly character, the player can switch control over to that character, which is necessary for using some of their abilities to complete puzzles. Studs can be found by finding them, smashing or using the force on certain objects, or defeating enemies. Players will lose studs however if their character is destroyed (as opposed to losing lives). These studs can be spent on unlocking new characters for Free Play mode.',
+'PS2;XBOX;PC;', 2 , true , 'Action;Platform;' , STR_TO_DATE('03/29/2005', '%m/%d/%Y') , 'Traveller\'s Tales' , 'Eidos' , 
+'http://thegamesdb.net/banners/boxart/original/front/4840-1.jpg' , 'http://thegamesdb.net/banners/boxart/original/back/4840-1.jpg' , 
+'http://thegamesdb.net/banners/clearlogo/4840.png' , NULL , 5.99 , 0.00
 );
